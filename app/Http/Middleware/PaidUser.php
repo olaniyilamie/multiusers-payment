22efd88d186f9727d3bc7fcfcb5e23775ad54567
OLAMIDE <olaniyilamie@gmail.com>
2022-06-24 17:58:17 +0100

    debuging

--- app/Http/Middleware/PaidUser.php
+++ app/Http/Middleware/PaidUser.php
@@ -31,7 +31,7 @@ class PaidUser
     // {
     //         return redirect()->route('user');
     //     }  
-        dd(Auth::user()->role == 'free_user',Auth::user()->role == 'paid_user',$next,$request);
-        return $next($request);
+        //dd(Auth::user()->role == 'free_user',Auth::user()->role == 'paid_user',$next,$request);
+        return $next('/dashboard');
     }
 }
